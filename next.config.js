b37ab054ad71caba8172365a779403885b2a3e8c
Tom Klaver <tomklav@gmail.com>
2019-08-14 10:34:34 +0200

    add cors-anywhere proxy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 const withSass = require('@zeit/next-sass');
 module.exports = withSass({
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    // mySecret: 'secret',
+    // secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // seedorfGraphQLUrl: 'https://api.sportyspots.com/graphql',
+    seedorfGraphQLUrl: 'http://localhost:8080/https://api.sportyspots.com/graphql',
+    seedorfHost: 'https://api.sportyspots.com',
+    googleMapsKey: process.env.GOOGLE_MAPS_KEY,
+  },
 });
 
 // const withImages = require('next-images')
