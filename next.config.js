8ffc3f677bb514e6a567c700c6de580ee5e3ff9d
Tom Klaver <tomklav@gmail.com>
2019-08-14 12:51:56 +0200

    remove publicRuntimeConfig from next config

--- next.config.js
+++ next.config.js
@@ -5,13 +5,6 @@ module.exports = withSass({
     // mySecret: 'secret',
     // secondSecret: process.env.SECOND_SECRET, // Pass through env variables
   },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    // seedorfGraphQLUrl: 'https://api.sportyspots.com/graphql',
-    seedorfGraphQLUrl: 'http://localhost:8080/https://api.sportyspots.com/graphql',
-    seedorfHost: 'https://api.sportyspots.com',
-    googleMapsKey: process.env.GOOGLE_MAPS_KEY,
-  },
   env: {
     seedorfGraphQLUrl: 'https://api.sportyspots.com/graphql',
   },
