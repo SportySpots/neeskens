132052c7ecc2598571eaf03b82066c87f2e68983
Tom Klaver <tomklav@gmail.com>
2019-08-14 17:23:12 +0200

    fix date/timezone for game start time

--- components/Activityimage/index.tsx
+++ components/Activityimage/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Game } from '../../types/game';
 
+import moment from 'moment';
+import 'moment-timezone';
+
 interface IProps {
   game: Game;
 }
@@ -10,11 +13,14 @@ const ActivityImage = ({ game }: IProps) => {
   if (!firstImage) {
     return null;
   }
+
+  const localStartTime = moment(game.start_time).tz('CET');
+
   return (
     <div className="rounded-lg shadow mb-8 ">
       <div className="absolute rounded-br-lg rounded-tl-lg bg-notify-100 w-24">
         <p className="pt-2 px-4 text-2xl font-bold text-chalk text-center">
-          {game.start_time}
+          {localStartTime.format('MMMM Do')}
         </p>
       </div>
       <img className="rounded-lg" src={firstImage.image} alt="activity image" />
