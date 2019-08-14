52b07a06ac2ace3ad3668cceff51b7156d0ab278
Tom Klaver <tomklav@gmail.com>
2019-08-14 14:05:19 +0200

    setup types

--- pages/games/[id].tsx
+++ pages/games/[id].tsx
@@ -2,22 +2,24 @@ import * as React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { NextPageContext } from 'next';
 import GET_GAME_DETAILS from '../../GraphQL/Games/Queries/GET_GAME_DETAILS';
+import { Game } from '../../types/game';
 
 interface IProps {
   id: string;
 }
 
 const game = (props: IProps) => {
-  const { data } = useQuery(GET_GAME_DETAILS, { variables: { uuid: props.id } });
+  const query = useQuery<Game>(GET_GAME_DETAILS, { variables: { uuid: props.id } });
+  const game = query.data;
 
-  if (!data) {
+  if (!game) {
     return (
       <div>Not found... {props.id}</div>
     );
   }
   return (
     <div>
-      {data && JSON.stringify(data.game)}
+      {game && JSON.stringify(game)}
     </div>
   );
 };
