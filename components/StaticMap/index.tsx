70af10dc0d88fb78c15eb96e8c5f0972a3d5c8f6
Tom Klaver <tomklav@gmail.com>
2019-08-14 16:52:03 +0200

    add StaticMap & some types

---
+++ components/StaticMap/index.tsx
@@ -0,0 +1,33 @@
+interface IProps {
+  coords: {
+    lat: number;
+    lng: number;
+  };
+}
+
+const StaticMap = (props: IProps) => {
+  const coords = props.coords;
+
+  const baseURL = 'https://maps.googleapis.com/maps/api/staticmap';
+  const coordsString = `${coords.lat},${coords.lng}`;
+  const params: { [label: string]: any } = {
+    center: coordsString,
+    zoom: 13,
+    scale: 1,
+    size: '600x300',
+    maptype: 'roadmap',
+    format: 'png',
+    visual_refresh: true,
+    markers: 'color:0x00ff00%7Clabel:S%7C' + coordsString,
+    markers_size: 'mid',
+    key: process.env.googleMapsAPIKey,
+  };
+
+  const queryString = Object.keys(params).map((key) => `${key}=${params[key]}`).join('&');
+
+  return (
+    <img alt="map" src={baseURL + '?' + queryString}/>
+  );
+};
+
+export default StaticMap;
--- types/spot.ts
+++ types/spot.ts
@@ -1,1 +1,24 @@
-export type Spot = any;
+import { UUID } from './utils';
+import { Game } from './game';
+import { Sport } from './sport';
+
+export interface Address {
+  uuid: UUID;
+  lat: number;
+  lng: number;
+}
+
+export interface Image {
+  image: string;
+}
+
+export interface Spot {
+  id: string;
+  uuid: UUID;
+  name: string;
+  images: Image[];
+  games: Game[];
+  amenities: any[];
+  sports: Sport[];
+  address: Address;
+}
