c86336e6bf38799ab9f7b81d1ae2670f57919650
Sezayi <arslan.sezayi@gmail.com>
2019-08-21 13:14:01 +0200

    add font styling

--- components/ActivityAttendees/index.tsx
+++ components/ActivityAttendees/index.tsx
@@ -9,7 +9,7 @@ interface IProps {
 const ActivityAttendees = ({ game }: IProps) => {
     return (
         <div className="bg-chalk rounded-lg p-8">
-            <h2 className="pb-4">Attendees</h2>
+            <h2 className="font-sans text-3xl pb-4">Attendees</h2>
             <ul>
                 {game.attendees
                     .filter(attendee => attendee.status === 'ATTENDING')
