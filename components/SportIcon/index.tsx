3b0edf27407a049305d7fd1663e15361e18beb37
Tom Klaver <tomklav@gmail.com>
2019-08-21 11:57:10 +0200

    add SportIcon component

---
+++ components/SportIcon/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+
+interface IProps {
+    sport: string
+}
+
+const sportToFilename: { [key: string]: string } = {
+    basketball: 'basketball.png',
+    beach_volleyball: 'beach_volleyball.png',
+    soccer: 'soccer.png',
+    boules: 'boules.png',
+    skating: 'skating.png',
+    table_tennis: 'table_tennis.png',
+    tennis: 'tennis.png',
+    bootcamp: 'bootcamp.png',
+}
+
+const defaultSport = 'soccer'
+
+const SportIcon = ({ sport }: IProps) => {
+    const iconFilename = sportToFilename[sport] || sportToFilename[defaultSport]
+    return <img alt="Sport Icon" src={`/static/icons/${iconFilename}`} />
+}
+
+export default SportIcon
+
+// use as:
+// <SportIcon sport="soccer" />
