3665b7d238c6ed128b96eb382fc375c6256ef773
Sezayi <arslan.sezayi@gmail.com>
2019-08-21 11:32:54 +0200

    changes

--- components/ActivityDetails/index.tsx
+++ components/ActivityDetails/index.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { Game } from "../../types/game";
+import React from 'react'
+import { Game } from '../../types/game'
 
 interface IProps {
-  game: Game;
+    game: Game
 }
 
 const ActivityDetails = ({ game }: IProps) => {
-  return (
-    <div className="bg-chalk rounded-lg p-8 mb-8">
-      <h2 className="pb-4">{game.name}</h2>
-      <div>
-        <div>
-          <icon />
-          <p>Hier komen details</p>
+    return (
+        <div className="bg-chalk rounded-lg p-8 mb-8">
+            <h2 className="pb-4">{game.name}</h2>
+            <div>
+                <div>
+                    <icon />
+                    <p>Hier komen details</p>
+                </div>
+                <div>
+                    <icon />
+                    <p>Hier komen details</p>
+                </div>
+                <div>
+                    <icon />
+                    <p>Hier komen details</p>
+                </div>
+            </div>
         </div>
-        <div>
-          <icon />
-          <p>Hier komen details</p>
-        </div>
-        <div>
-          <icon />
-          <p>Hier komen details</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default ActivityDetails;
+export default ActivityDetails
