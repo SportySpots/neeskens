ac259928643bc34fec0befcca1a2e2a6aa586aea
Sezayi <arslan.sezayi@gmail.com>
2019-08-21 15:58:29 +0200

    update layout download button component

--- components/DownloadApp/index.tsx
+++ components/DownloadApp/index.tsx
@@ -7,19 +7,25 @@ const DownloadApp = () => {
                 <h2 className="text-center lg:text-left font-sans text-2xl">
                     Download the app and join this activity
                 </h2>
-                <div className="flex flex-row mt-4 mb-4 justify-center lg:justify-start">
-                    <a href="https://itunes.apple.com/nl/app/sportyspots/id1391625376">
+                <div className="flex flex-col lg:flex-row mt-4 justify-center lg:justify-start">
+                    <a
+                        className="flex justify-center"
+                        href="https://itunes.apple.com/nl/app/sportyspots/id1391625376"
+                    >
                         <img
                             src="/static/Appstore.svg"
                             alt="AppImage"
-                            className="mt-4 mr-4 "
+                            className="my-2 lg:mr-8"
                         />
                     </a>
-                    <a href="https://play.google.com/store/apps/details?id=com.sportyspots.android">
+                    <a
+                        className="flex justify-center"
+                        href="https://play.google.com/store/apps/details?id=com.sportyspots.android"
+                    >
                         <img
                             src="/static/Playstore.svg"
                             alt="AppImage"
-                            className="mt-4"
+                            className="my-2"
                         />
                     </a>
                 </div>
