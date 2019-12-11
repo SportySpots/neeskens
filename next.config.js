b54a6e5040865aa6fa1bbb34883482f4c3ffc9ef
Tom Klaver <tomklav@gmail.com>
2019-12-11 15:06:40 +0100

    setup import aliases, tslint->eslint, adds leaflet packages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass({
+const withCss = require('@zeit/next-css');
+
+module.exports = withCss(withSass({
+  webpack (config, options) {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      }
+    })
+
+    return config
+  },
   serverRuntimeConfig: {
     // Will only be available on the server side
     // mySecret: 'secret',
@@ -11,7 +26,7 @@ module.exports = withSass({
     googleMapsAPIKey: 'AIzaSyC99uVW_9nOJ32T7BY3Yeqt0iMWqOrGuAQ',
     googleAnalyticsID: 'UA-110079868-1',
   },
-});
+}));
 
 // const withImages = require('next-images')
 // module.exports = withImages()
