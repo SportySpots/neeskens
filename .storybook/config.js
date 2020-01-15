07700b0b63554edeb25efea2c1dbdf589cbbaf1d
Tom Klaver <tomklav@gmail.com>
2020-01-15 16:11:16 +0100

    storybook css fix

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import '../.next/static/css/styles.chunk.css';
+// import '../.next/static/css/styles.chunk.css';
 import { configure } from '@storybook/react';
 // automatically import all files ending in *.stories.tsx
 // configure(require.context('../stories', true, /\.stories\.tsx?$/), module);
 configure(require.context('../', true, /\.stories\.tsx?$/), module);
+
+function requireAll(r) { r.keys().forEach(r); }
+requireAll(require.context('../.next/static/css', true, /\.css$/));
