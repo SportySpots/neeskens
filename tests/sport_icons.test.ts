cdab49a4ae307a5afcc954dcaae2f5e7bceba5be
Tom Klaver <tomklav@gmail.com>
2020-01-15 13:30:50 +0100

    icons, filters styling, jest setup

---
+++ tests/sport_icons.test.ts
@@ -0,0 +1,22 @@
+import { Sports } from '../const/sports'
+import fs from 'fs';
+import path from 'path'
+
+// project path
+const rootPath = (global as any).rootDir;
+
+const sportIconsPath = path.join(rootPath, 'public', 'static', 'sport_icons');
+
+describe('sport icons', () => {
+    it('as many icons as sports', () => {
+        expect(fs.readdirSync(sportIconsPath).length).toEqual(Sports.length);
+    });
+    describe('exist for each sport', () => {
+        Sports.forEach(sport => {
+            const expectedFileName = path.join(sportIconsPath, sport.category.toLowerCase() + '.svg')
+            it(`${sport.category} has an icon ${expectedFileName}`, () => {
+                expect(fs.existsSync(expectedFileName)).toBeTruthy();
+            })
+        })
+    })
+})
