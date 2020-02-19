d3d7d67baadeb35b327fc837cc2214fadcef6510
Tom Klaver <tomklav@gmail.com>
2020-02-19 15:24:41 +0100

    remove isProduction hack

--- pages/_document.js
+++ pages/_document.js
@@ -2,17 +2,7 @@ import React from 'react'
 import Document, { Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    // Check if in production
-    const isProduction = process.env.NODE_ENV === 'production';
-    const initialProps = await Document.getInitialProps(ctx);
-    // Pass isProduction flag back through props
-    return {...initialProps, isProduction};
-  }
-
   render() {
-    const { isProduction } = this.props;
-
     return (
       <html>
       <Head>
@@ -20,7 +10,7 @@ export default class MyDocument extends Document {
           href="https://fonts.googleapis.com/css?family=Rajdhani::400,500,600,700&display=swap"
           rel="stylesheet"
         />
-        { isProduction && (
+        { process.env.NODE_ENV === 'production' && (
           <>
             <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.googleAnalyticsID}`}/>
             <script dangerouslySetInnerHTML={{ __html: `
